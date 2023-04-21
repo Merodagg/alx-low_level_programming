2e537f8677ef2719ccc73b9f9bb43d21b3d660b1
meroda <merodagg@dengene.com>
2023-04-20 20:03:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
 /**
  * main - it is the main entry point
- *Return: 0 - (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
