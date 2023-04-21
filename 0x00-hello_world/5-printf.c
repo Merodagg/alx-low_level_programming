c1f71efcfbe1fd1c8885bceccf3528462a467967
meroda <merodagg@dengene.com>
2023-04-20 20:48:55 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
-  *main - A program that print a line using the printf function
-  *Return: 0 (success)
-  */
+ * main -Program that print a line using printf function
+* Return: 0
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
