afbe481d48914191ec34652247a222676763354d
meroda <merodagg@dengene.com>
2023-04-20 22:42:41 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
