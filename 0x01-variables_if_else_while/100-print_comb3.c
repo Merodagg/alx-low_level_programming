49ce638ce8bf533ab2adfb37bdfcf499aeb19800
meroda <merodagg@dengene.com>
2023-04-21 18:41:43 -0700

    print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,29 @@
 /**
 * main - print allpossible different combinations of two dgits.
 *
-* Return: Always 0
+* Return: Always 0.
 */
 int main(void)
 {
-	int digit1, digit2;
+int i;
+int j;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 0 && digit2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (i > j)
+continue;
+if (i == j)
+continue;
+putchar('0' + i);
+putchar('0' + j);
+if (i == 8)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
