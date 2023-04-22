67ed230dbf19c933ad465ad522affcb5899f847a
meroda <merodagg@dengene.com>
2023-04-21 19:51:17 -0700

    opo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,23 @@ int main(void)
 {
 	int p, q;
 
-for (p = 0; p <= 98; p++)
+for (p = 0; p <= 99; p++)
 {
-for (q = p + 1; q < 99; q++)
+for (q = p + 1; q <= 99; q++)
+{
+if (p < q && p != q)
 {
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
-if (p == 98 && q == 99)
-continue;
+if (p != 98 || q != 99)
+{
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
