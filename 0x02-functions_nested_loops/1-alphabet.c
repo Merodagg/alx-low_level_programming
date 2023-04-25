8e9de1f4b879a2d64956be052fafc5563e925746
meroda <merodagg@dengene.com>
2023-04-25 16:04:27 -0700

    task alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - the alphabet in lower case
+ *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
+int i;
+
+for (i=97; i <= 122; i++)
+{
+putchar(i);
+}
+putchar('\n');
 return (0);
 }
