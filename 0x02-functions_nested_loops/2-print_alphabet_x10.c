887b1c17cf8d7ebb65723445245333824bfa9d07
meroda <merodagg@dengene.com>
2023-04-25 16:36:36 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+ 
+ /**
+  * print_alphabet - the alphabet in lower case
+  *
+  * Return: Always 0.
+  */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 1; i <= 10; i++)
+{
+ for (j = 97; j <= 122; j++)
+{
+
+_putchar(i);
+}				  }
+_putchar('\n');
+}
+}
