0cda9847d6594d41a3af69fb13fd0fe940918184
meroda <merodagg@dengene.com>
2023-04-25 15:29:32 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
- #include "main.h"
+#include "main.h"
 
 /**
  * main - entry point
  * Return: always 0
  */
- int main(void)
-	 {
+int main(void)
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
