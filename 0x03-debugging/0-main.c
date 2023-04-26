e9b4dfa2520de09269cc7ba216a7449a712d4e26
meroda <merodagg@dengene.com>
2023-04-26 07:00:46 -0700

    negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,13 +2,13 @@
 
 /**
  * main - function for positive or negative
- * Return: negative or positive
+ * Return: 0
  */
 int main(void)
 {
-int i;
+	int i;
 
-i = 98;
-positive_or_negative(i);
-return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
