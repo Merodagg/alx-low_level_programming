e5264edfe5e330a99f8c1a49acf2aabb9a7a8747
meroda <merodagg@dengene.com>
2023-04-26 04:10:06 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * Add - add two numbers
+  * @n1: the first number
+  * @n2: the second number
+  * Return: integer value
+  */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
+
